06b2b9272a175d5f33847e7ffc91c61ffb01cf85
pavanreddykarnati7-tech <pavanreddykarnati7@gmail.com>
2026-05-30 12:26:52 +0530

    kkk

--- python/stringprb.py
+++ python/stringprb.py
@@ -1,2 +1,3 @@
 s=str(input())
 print("number of vowels :", s.count("a")+s.count("e")+s.count("i")+s.count("o")+s.count("u"))
+print("number of consonants :", len(s)-s.count("a")-s.count("e")-s.count("i")-s.count("o")-s.count("u")-s.count(" "))
